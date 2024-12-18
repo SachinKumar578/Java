7ed1be5acf7c125ab32b66dc71be62456c753820
SachinKumar578 <122079001+SachinKumar578@users.noreply.github.com>
2024-12-18 10:48:18 +0530

    Create HelloWorld.java

---
+++ HelloWorld.java
@@ -0,0 +1,22 @@
+// First Java Program
+
+public class HelloWorld{
+    public static void main(String[] args){
+        System.out.println("Hello World!");
+        System.out.print("Sum of these numbers is : ");
+        int num1 = 12 ;
+        int num2 = 18 ;
+        int num3 = 15 ;
+        int sum = num1 + num2 + num3 ;
+        System.out.println(sum);
+
+
+    }
+}
+
+
+// Naming conventions in Java
+// add two numbers
+
+// AddTwoNumbers -> PascalNamingConvention ,, for class we use this naming convention
+// addTwoNumbers -> CamelCaseNamingConvention ,, for function we use this naming convention 
