e6fe91e81f811b6d29e052e90a24eb06cfab1b36
SachinKumar578 <122079001+SachinKumar578@users.noreply.github.com>
2024-12-19 12:43:51 +0530

    Create PracticeSet1.java

---
+++ PracticeSet1.java
@@ -0,0 +1,47 @@
+import java.util.Scanner;
+
+public class PracticeSet1 {
+    public static void main(String[] args) {
+        // Question1:- WAP to print sum of three numbers in java.
+        //  int a = 5 ;
+        //  int b = 10 ;
+        //  int c = 15 ;
+
+        //  int sum = a + b + c ;
+        //  System.out.println(sum);
+        
+        
+        // Question2:- WAP to calculate cgpa using marks of 3 subjects in java.
+        // float subject1 = 85.65f ;
+        // float subject2 = 75.50f ;
+        // float subject3 = 90.85f ;
+
+        // float cgpa = ((subject1 + subject2 + subject3)/30) ;
+        // System.out.println(cgpa);
+
+
+        // Question3:- WAP which asks the user to eneter his/her name and greets 
+        //             them with "Hello <name> have a good day", text.
+        // System.out.println("What is your name ?");
+        // Scanner sc = new Scanner(System.in);
+        // String name = sc.next();
+        // System.out.println("Hello, " + name + " have a good day.");
+
+
+        // Question4:- WAP to convert kilometers to miles.
+        // System.out.print("Enter kilometers to convert in miles : ");
+        // Scanner sc = new Scanner(System.in);
+        // float kilometers = sc.nextFloat();
+        // float miles = kilometers * 0.62137f ;
+        // System.out.println(kilometers + " kilometers in miles is : " + miles + " miles");
+        
+        
+
+        // Question5:- WAP to detect whether a no. enetered by user is integer or not.
+        System.out.print("Enter your number : ");
+        Scanner sc = new Scanner(System.in);
+        System.out.println(sc.hasNextInt());
+
+
+    }
+}
