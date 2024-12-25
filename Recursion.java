ccd11e1a1bdaa19f6c54edee9728645f3d2b498a
SachinKumar578 <122079001+SachinKumar578@users.noreply.github.com>
2024-12-25 15:25:18 +0530

    Add files via upload

---
+++ Recursion.java
@@ -0,0 +1,20 @@
+public class Recursion {
+    // factorial(n) = n * n-1 * ...... * 1
+    // factorial(5) = 5 * 4 * 3 * 2 * 1 = 120 
+    // factorial(n) = n * factorial(n-1)
+
+    static int factorial(int n){
+        if (n==0 || n==1) {
+            return 1 ;
+            
+        }
+        else{
+            return n * factorial(n-1) ;
+        }
+
+    }
+    public static void main(String[] args) {
+        int x = 4 ;
+        System.out.println("The factorial of value n is : " + factorial(x));
+    }
+}
